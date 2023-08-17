0a74a8671e1e48eba2764390d7f48e49da6ab2b4
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-08-17 10:44:03 +0300

    feat: add base-table2

--- packages/cv-gen/src/app/projects/projects.module.ts
+++ packages/cv-gen/src/app/projects/projects.module.ts
@@ -5,6 +5,7 @@ import { ProjectRoutingModule } from './projects-routing.module';
 import { ProjectListPageComponent } from './pages/projects-list/projects-list.page.component';
 import { CreateProjectPageComponent } from './pages/create-project/create-project.page.component';
 import { EditProjectPageComponent } from './pages/edit-project/edit-project.page.component';
+import { BaseTable2Component } from '../shared/components/base-table2/base-table2.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,11 @@ import { EditProjectPageComponent } from './pages/edit-project/edit-project.page
     CreateProjectPageComponent,
     EditProjectPageComponent,
   ],
-  imports: [CommonModule, ProjectRoutingModule, ProjectRoutingModule],
+  imports: [
+    CommonModule,
+    ProjectRoutingModule,
+    ProjectRoutingModule,
+    BaseTable2Component,
+  ],
 })
 export class ProjectsModule {}
--- packages/cv-gen/src/app/shared/components/base-table2/base-table2.component.ts
+++ packages/cv-gen/src/app/shared/components/base-table2/base-table2.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'cv-gen-base-table2',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TableModule],
   templateUrl: './base-table2.component.html',
   styleUrls: ['./base-table2.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
