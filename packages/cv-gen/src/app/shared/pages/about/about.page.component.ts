d971fd65ca7ee9161d02aaba88ac6958425b8d40
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-08-23 21:05:13 +0300

    feat: add styles for about page

--- packages/cv-gen/src/app/shared/pages/about/about.page.component.ts
+++ packages/cv-gen/src/app/shared/pages/about/about.page.component.ts
@@ -1,10 +1,12 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'cv-gen-about.page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule, RouterModule],
   templateUrl: './about.page.component.html',
   styleUrls: ['./about.page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
